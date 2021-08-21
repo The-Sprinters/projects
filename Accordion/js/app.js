dc390af616fa164d4a07d29807915eae86e4d35b
amruta-kashikar <amrutakashikar2@gmail.com>
2021-08-21 18:38:50 +0530

    Added accordion code

---
+++ Accordion/js/app.js
@@ -0,0 +1,19 @@
+var acc = document.getElementsByClassName("accordion");
+// var mus = document.getElementsByClassName("mus");
+// var plus = document.getElementsByClassName("plus");
+
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+
+    })
+}
+
